8b8101c104b4fea2a5c520417572b8ea2663c33c
Syntaxfehlr <152663358+Syntaxfehlr@users.noreply.github.com>
2024-06-11 00:02:04 +0200

    changed variables

--- Individual_implementation/Carl/RP.py
+++ Individual_implementation/Carl/RP.py
@@ -5,15 +5,15 @@ import matplotlib.pyplot as plt
 signal = np.sin(np.linspace(0, 4 * np.pi, 100))
 
 threshold = 0.05 
-recurrence_matrix = np.zeros((len(signal), len(signal)))
+M = np.zeros((len(signal), len(signal)))
 
 for i in range(len(signal)):
     for j in range(len(signal)):
         distance = abs(signal[i] - signal[j])
         if distance < threshold:
-            recurrence_matrix[i, j] = 1
+            M[i, j] = 1
 
 
-plt.imshow(recurrence_matrix, cmap='binary', origin='lower')
+plt.imshow(M, cmap='binary', origin='lower')
 plt.title('Recurrence Plot')
 plt.show()
