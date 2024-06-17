e22496bfc8abd5f21543708c1fa306110f95a88b
akashries <akashries@gmail.com>
2024-06-17 13:08:19 +0200

    includes manually calculated rqas

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -5,21 +5,18 @@ from scipy.spatial.distance import pdist, squareform
 from attractor_functions import *
 from rqa_functions import *
 import pandas as pd
-
+import numpy as np
 
 class LivePlotApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Live Data and Recurrence Plot")
-
         self.create_tabs()
         self.create_home_tab()
         self.create_functions_tab()
         self.create_data_tab()
 
-
     def create_tabs(self):
-        # Configure tab system
         self.notebook = ttk.Notebook(root)
         self.home_tab = ttk.Frame(self.notebook)
         self.function_tab = ttk.Frame(self.notebook)
@@ -31,41 +28,32 @@ class LivePlotApp:
         self.notebook.pack(expand=1, fill='both')
 
     def create_home_tab(self):
-        # Title label
-        self.title_label = ttk.Label(self.home_tab, text="Welcome to Live Data and Recurrence Plot GUI",
-                                     font=("Helvetica", 16))
+        self.title_label = ttk.Label(self.home_tab, text="Welcome to Live Data and Recurrence Plot GUI", font=("Helvetica", 16))
         self.title_label.pack(pady=10)
 
-        # Textbox explaining what the GUI does
         self.info_text = tk.Text(self.home_tab, wrap='word', height=10, width=50)
         self.info_text.insert(tk.END,
                               "This GUI allows you to visualize different chaotic systems and analyze their recurrence plots. "
                               "You can start/stop/reset the live plotting of selected functions and display histograms of recurrence quantification analysis (RQA) measures.")
-        self.info_text.config(state=tk.DISABLED)  # Make the textbox read-only
+        self.info_text.config(state=tk.DISABLED)
         self.info_text.pack(pady=10)
 
-        # Buttons to navigate to the other tabs
-        self.btn_functions_tab = ttk.Button(self.home_tab, text="Go to Plotting Functions",
-                                            command=lambda: self.notebook.select(self.function_tab))
+        self.btn_functions_tab = ttk.Button(self.home_tab, text="Go to Plotting Functions", command=lambda: self.notebook.select(self.function_tab))
         self.btn_functions_tab.pack(pady=5)
 
-        self.btn_data_tab = ttk.Button(self.home_tab, text="Go to Plotting Data",
-                                       command=lambda: self.notebook.select(self.data_tab))
+        self.btn_data_tab = ttk.Button(self.home_tab, text="Go to Plotting Data", command=lambda: self.notebook.select(self.data_tab))
         self.btn_data_tab.pack(pady=5)
 
     def create_functions_tab(self):
-        # Configure grid layout in the function_tab
         self.function_tab.columnconfigure(0, weight=1)
         self.function_tab.columnconfigure(1, weight=1)
         self.function_tab.rowconfigure(0, weight=1)
         self.function_tab.rowconfigure(1, weight=1)
 
-        # Create a figure and three subplots
         self.fig_ps_func = plt.Figure()
         self.fig_comp_func = plt.Figure()
         self.fig_rp_func = plt.Figure()
 
-        # Embedding the Matplotlib figures into Tkinter canvas
         self.canvas_ps_func = FigureCanvasTkAgg(self.fig_ps_func, master=self.function_tab)
         self.canvas_ps_func.get_tk_widget().grid(row=0, column=1, sticky='nsew')
 
@@ -75,7 +63,6 @@ class LivePlotApp:
         self.canvas_rp_func = FigureCanvasTkAgg(self.fig_rp_func, master=self.function_tab)
         self.canvas_rp_func.get_tk_widget().grid(row=1, column=1, sticky='nsew')
 
-        # Make frame for buttons
         self.command_window_func = ttk.Frame(self.function_tab)
         self.command_window_func.grid(row=0, column=0, sticky='nsew')
 
@@ -87,7 +74,6 @@ class LivePlotApp:
         self.command_window_func.rowconfigure(2, weight=1)
         self.command_window_func.rowconfigure(3, weight=1)
 
-        # Add Start, Stop, and Reset buttons to control the live plotting
         self.btn_start_func = ttk.Button(self.command_window_func, text="Start", command=self.start_func)
         self.btn_start_func.grid(row=0, column=0)
 
@@ -97,12 +83,9 @@ class LivePlotApp:
         self.btn_reset_func = ttk.Button(self.command_window_func, text="Reset", command=self.reset_func)
         self.btn_reset_func.grid(row=2, column=0)
 
-        # Add button to open histogram window
-        self.btn_histogram_func = ttk.Button(self.command_window_func, text="Show Histogram",
-                                             command=lambda: show_histogram(self))
+        self.btn_histogram_func = ttk.Button(self.command_window_func, text="Show Histogram", command=lambda: show_histogram(self))
         self.btn_histogram_func.grid(row=3, column=0)
 
-        # Add drop down menu to select function
         self.selected_option_func = tk.StringVar()
         functions = ["Lorenz", "Chua", "Rossler", "Chen"]
         self.selected_option_func.set(functions[0])
@@ -110,36 +93,29 @@ class LivePlotApp:
         self.dropdown_func.grid(row=0, column=1)
         self.selected_option_func.trace("w", self.on_select)
 
-        # Label to display RQA measures
         self.rqa_label_func = ttk.Label(self.command_window_func, text="RQA Measures will appear here")
         self.rqa_label_func.grid(row=0, column=2, rowspan=2)
 
-        # Variables to control the live plotting
         self.is_running = False
-        self.xyzs = np.array([[0., 1., 1.05]])  # Initialize xyzs with an initial value
+        self.xyzs = np.array([[0., 1., 1.05]])
         self.time_step = 0
         self.dt = 0.01
 
     def create_data_tab(self):
-        # Configure grid layout in the function_tab
         self.data_tab.columnconfigure(0, weight=1)
         self.data_tab.columnconfigure(1, weight=1)
-        # self.data_tab.columnconfigure(2, weight=1)
         self.data_tab.rowconfigure(0, weight=1)
         self.data_tab.rowconfigure(1, weight=1)
 
-        # Create a figure and three subplots
         self.fig_data = plt.Figure()
         self.fig_rp_data = plt.Figure()
 
-        # Embedding the Matplotlib figures into Tkinter canvas
         self.canvas_comp_data = FigureCanvasTkAgg(self.fig_data, master=self.data_tab)
         self.canvas_comp_data.get_tk_widget().grid(row=1, column=0, sticky='nsew')
 
         self.canvas_rp_data = FigureCanvasTkAgg(self.fig_rp_data, master=self.data_tab)
         self.canvas_rp_data.get_tk_widget().grid(row=1, column=1, sticky='nsew')
 
-        # Make frame for buttons
         self.command_window_data = ttk.Frame(self.data_tab)
         self.command_window_data.grid(row=0, column=0, columnspan=2, sticky='nsew')
 
@@ -153,22 +129,18 @@ class LivePlotApp:
         self.command_window_data.rowconfigure(2, weight=1)
         self.command_window_data.rowconfigure(3, weight=1)
 
-        # Add Start, Stop, and Reset buttons to control the live plotting
         self.btn_run_data = ttk.Button(self.command_window_data, text="Run", command=self.run_data)
         self.btn_run_data.grid(row=0, column=0)
 
         self.btn_reset_data = ttk.Button(self.command_window_data, text="Reset", command=self.reset_data)
         self.btn_reset_data.grid(row=1, column=0)
 
-        # Add button to open histogram window
-        self.btn_histogram_data = ttk.Button(self.command_window_data, text="Show Histogram",
-                                          command=lambda: show_histogram(self))
+        self.btn_histogram_data = ttk.Button(self.command_window_data, text="Show Histogram", command=lambda: show_histogram(self))
         self.btn_histogram_data.grid(row=3, column=0)
 
-        # Add text inputs for embedding dimension (m), time delay (T), and threshold (e)
-        self.embedding_dim_var = tk.IntVar(value=1)  # Initial value for m
-        self.time_delay_var = tk.IntVar(value=1)  # Initial value for T
-        self.threshold_var = tk.DoubleVar(value=1)  # Initial value for e
+        self.embedding_dim_var = tk.IntVar(value=1)
+        self.time_delay_var = tk.IntVar(value=1)
+        self.threshold_var = tk.DoubleVar(value=1)
 
         self.embedding_dim_label = ttk.Label(self.command_window_data, text="Embedding Dimension (m):")
         self.embedding_dim_label.grid(row=0, column=1, sticky='e')
@@ -185,16 +157,13 @@ class LivePlotApp:
         self.threshold_input = ttk.Entry(self.command_window_data, textvariable=self.threshold_var)
         self.threshold_input.grid(row=2, column=2, sticky='w')
 
-        # Label to display RQA measures
         self.rqa_label_data = ttk.Label(self.command_window_data, text="RQA Measures will appear here")
         self.rqa_label_data.grid(row=0, column=3, rowspan=2)
 
-
     def toggle_buttons(self, state):
-            # Toggle all buttons except start and stop
-            self.btn_reset_func.config(state=state)
-            self.btn_histogram_func.config(state=state)
-            self.dropdown_func.config(state=state)
+        self.btn_reset_func.config(state=state)
+        self.btn_histogram_func.config(state=state)
+        self.dropdown_func.config(state=state)
 
     def plot_data(self):
         if not self.is_running:
@@ -209,18 +178,13 @@ class LivePlotApp:
         T = int(self.time_delay_var.get())
         epsilon = int(self.threshold_var.get())
 
-        # Construct embedded vectors
         num_vectors = len(self.data) - (m - 1) * T
         vectors = np.array([self.data[t:t + m * T:T] for t in range(num_vectors)]).reshape(-1,1)
 
-        # Find recurrence points using distance between embedded vectors
         self.D = squareform(pdist(vectors, metric='euclidean'))
-        hit = np.argwhere(self.D < epsilon)
+        recurrence_matrix = self.D < epsilon
+        x_rec, y_rec = np.argwhere(recurrence_matrix).T
 
-        # Extract x and y coordinates of points of recurrence
-        x_rec, y_rec = hit[:, 0], hit[:, 1]
-
-        # Plot data
         self.fig_data.clear()
         ax_data = self.fig_data.add_subplot(111)
         ax_data.plot(self.data, lw=0.5)
@@ -229,7 +193,6 @@ class LivePlotApp:
         ax_data.set_ylabel("Y Axis")
         self.canvas_comp_data.draw()
 
-        # Plot recurrence plot
         self.fig_rp_data.clear()
         ax_rp_data = self.fig_rp_data.add_subplot(111)
         ax_rp_data.scatter(x_rec, y_rec, s=1)
@@ -238,12 +201,12 @@ class LivePlotApp:
         ax_rp_data.set_ylabel("Vector Index")
         self.canvas_rp_data.draw()
 
-        # Calculate and display RQA measures
         rqa_measures_data = calculate_rqa_measures_pyrqa(vectors, epsilon)
-        # det2, lam2 = self.calculate_manual_det_lam(recurrence_matrix)
-        # rqa_measures["DET2"] = det2
-        # rqa_measures["LAM2"] = lam2
-        display_rqa_measures(self, self.rqa_label_data, rqa_measures_data)
+        det2, lam2, lmax2 = calculate_manual_det_lam_lmax(recurrence_matrix)
+        rqa_measures_data["DET2"] = det2
+        rqa_measures_data["LAM2"] = lam2
+        rqa_measures_data["Lmax2"] = lmax2
+        display_rqa_measures(self.rqa_label_data, rqa_measures_data)
 
     def update_plot(self):
         if not self.is_running:
@@ -254,7 +217,7 @@ class LivePlotApp:
         elif selected_function_name == 'chua':
             new_point = self.xyzs[-1] + chua(self.xyzs[-1]) * self.dt
         else:
-            new_point = self.xyzs[-1]  # Default, no change
+            new_point = self.xyzs[-1]
 
         self.xyzs = np.append(self.xyzs, [new_point], axis=0)
         self.fig_ps_func.clear()
@@ -302,11 +265,11 @@ class LivePlotApp:
             ax_rp_func.set_xlabel("Vector Index")
             ax_rp_func.set_ylabel("Vector Index")
             rqa_measures_func = calculate_rqa_measures_pyrqa(vectors, epsilon)
-            #det2, lam2 = self.calculate_manual_det_lam(recurrence_matrix)
-            #rqa_measures["DET2"] = det2
-            #rqa_measures["LAM2"] = lam2
-            display_rqa_measures(self, self.rqa_label_func, rqa_measures_func)
-
+            det2, lam2, lmax2 = calculate_manual_det_lam_lmax(recurrence_matrix)
+            rqa_measures_func["DET2"] = det2
+            rqa_measures_func["LAM2"] = lam2
+            rqa_measures_func["Lmax2"] = lmax2
+            display_rqa_measures(self.rqa_label_func, rqa_measures_func)
 
     def start_func(self):
         if not self.is_running:
@@ -321,7 +284,7 @@ class LivePlotApp:
 
     def reset_func(self):
         self.is_running = False
-        self.xyzs = np.array([[0., 1., 1.05]])  # Reset the initial value
+        self.xyzs = np.array([[0., 1., 1.05]])
         self.fig_ps_func.clear()
         self.fig_comp_func.clear()
         self.fig_rp_func.clear()
--- GUI/rqa_functions.py
+++ GUI/rqa_functions.py
@@ -34,7 +34,6 @@ def calculate_rqa_measures_pyrqa(vectors, epsilon):
 
 def calculate_manual_det_lam_lmax(recurrence_matrix):
     def calculate_det(recurrence_matrix):
-        # Calculate diagonal line lengths
         diagonals = [np.diagonal(recurrence_matrix, offset=i) for i in range(-recurrence_matrix.shape[0] + 1, recurrence_matrix.shape[1])]
         diag_lengths = [len(list(group)) for diag in diagonals for value, group in groupby(diag) if value == 1]
         diag_lengths = [length for length in diag_lengths if length >= 2]
@@ -43,7 +42,6 @@ def calculate_manual_det_lam_lmax(recurrence_matrix):
         return sum(diag_lengths) / np.sum(recurrence_matrix)
 
     def calculate_lam(recurrence_matrix):
-        # Calculate vertical line lengths
         verticals = recurrence_matrix.T
         vert_lengths = [len(list(group)) for col in verticals for value, group in groupby(col) if value == 1]
         vert_lengths = [length for length in vert_lengths if length >= 2]
@@ -52,7 +50,7 @@ def calculate_manual_det_lam_lmax(recurrence_matrix):
         return sum(vert_lengths) / np.sum(recurrence_matrix)
 
     def calculate_lmax(recurrence_matrix):
-        diagonals = [np.diagonal(recurrence_matrix, offset=i) for i in range(-recurrence_matrix.shape[0] + 1, recurrence_matrix.shape[1])]
+        diagonals = [np.diagonal(recurrence_matrix, offset=i) for i in range(-recurrence_matrix.shape[0] + 1, recurrence_matrix.shape[1]) if i != 0]
         diag_lengths = [len(list(group)) for diag in diagonals for value, group in groupby(diag) if value == 1]
         diag_lengths = [length for length in diag_lengths if length >= 2]
         if len(diag_lengths) == 0:
@@ -65,7 +63,8 @@ def calculate_manual_det_lam_lmax(recurrence_matrix):
 
     return det2, lam2, lmax2
 
-def display_rqa_measures(self, rqa_label, rqa_measures):
+
+def display_rqa_measures(rqa_label, rqa_measures):
     text = "\n".join([f"{k}: {v:.4f}" for k, v in rqa_measures.items()])
     rqa_label.config(text=text)
 
