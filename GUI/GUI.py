1316a42fca20b255a5f01181ccaab29bcfad8746
martinaKiewek <77066406+martinaKiewek@users.noreply.github.com>
2024-06-18 19:05:23 +0200

    (update) mainly changes to how layout is coded also added comments

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,3 +1,8 @@
+# Exploratory GUI for interacting with Recurrence Plots
+# Maastricht University, Circular Engineering
+# June, 2024
+
+# Imports
 import tkinter as tk
 from tkinter import ttk
 from home_tab import homeTab
@@ -6,8 +11,11 @@ from data_tab import dataTab
 
 class LivePlotApp:
     def __init__(self, root):
+        # window set up
         self.root = root
         self.root.title("Live Data and Recurrence Plot")
+
+        # tab set up
         self.notebook = ttk.Notebook(root)
         self.notebook.pack(expand=1, fill='both')
         self.home_tab = homeTab(self.notebook)
