7bc26d4d2663799212bdb1fee43a527165db79a7
Syntaxfehlr <152663358+Syntaxfehlr@users.noreply.github.com>
2024-06-25 13:57:08 +0200

    tool

---
+++ Classifier/Tool.py
@@ -0,0 +1,7 @@
+""" 
+1. Load data (eg. 0 - 1 000 000)
+2. use first 50 000 samples for training
+3. create timeseries based on healthy -> fault (selectable)
+4. simulate live data acquisition -> processing -> classification
+5. plot per window: timeseries, recurrence plot, rqa graph (customizable), classification
+"""
