aebdc2dc7ca85c37adc0cf8148123cc8b3589b63
Syntaxfehlr <152663358+Syntaxfehlr@users.noreply.github.com>
2024-06-25 14:00:27 +0200

    list of timeseries

--- Classifier/preprocessing.py
+++ Classifier/preprocessing.py
@@ -12,43 +12,7 @@ def sliding_window_view(arr, window_size, step):
     strides = (step * arr.strides[0], arr.strides[0])
     return np.lib.stride_tricks.as_strided(arr, shape=shape, strides=strides)
 
-def prepare_datasets_multi_class(healthy, race_fault, ball_fault, window_size, delay, feature_func):
-    # Create features for healthy data
-    healthy_windows = sliding_window_view(healthy, window_size, delay)
-    healthy_measures = np.apply_along_axis(feature_func, 1, healthy_windows)
-    print("Healthy measures shape:", healthy_measures.shape)
-    
-    # Create features for race fault data
-    race_fault_windows = sliding_window_view(race_fault, window_size, delay)
-    race_fault_measures = np.apply_along_axis(feature_func, 1, race_fault_windows)
-    print("Race fault measures shape:", race_fault_measures.shape)
-
-    # Create features for ball fault data
-    ball_fault_windows = sliding_window_view(ball_fault, window_size, delay) 
-    ball_fault_measures = np.apply_along_axis(feature_func, 1, ball_fault_windows)
-    print("Ball fault measures shape:", ball_fault_measures.shape)
-    
-    size = healthy_measures.shape[0]
-    # Label the data
-    healthy_labels = np.zeros(size)
-    race_fault_labels = np.ones(size)
-    ball_fault_labels = np.ones(size) * 2
-    
-    print("Labels shape:", (healthy_labels.shape, race_fault_labels.shape, ball_fault_labels.shape))
-    
-    # Combine the datasets
-    X = np.vstack((healthy_measures, race_fault_measures, ball_fault_measures))
-    y = np.hstack((healthy_labels, race_fault_labels, ball_fault_labels))
-    print("Concatenated dataset shape:", X.shape)
-    
-    # Split into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    print("Training set shape:", X_train.shape, "Testing set shape:", X_test.shape)
-    
-    return X_train, X_test, y_train, y_test
-
-
-def prepare_datasets_multi_class2(time_series, fault_names, window_size, delay, feature_func):
+def prepare_datasets_multi_class(time_series, fault_names, window_size, delay, feature_func):
     """
     All arrays in time_series should be the same length
     """
@@ -127,3 +91,40 @@ def scale_features(X_train, X_test):
     X_train_scaled = scaler.fit_transform(X_train)
     X_test_scaled = scaler.transform(X_test)
     return X_train_scaled, X_test_scaled
+
+""" 
+def prepare_datasets_multi_class(healthy, race_fault, ball_fault, window_size, delay, feature_func):
+    # Create features for healthy data
+    healthy_windows = sliding_window_view(healthy, window_size, delay)
+    healthy_measures = np.apply_along_axis(feature_func, 1, healthy_windows)
+    print("Healthy measures shape:", healthy_measures.shape)
+    
+    # Create features for race fault data
+    race_fault_windows = sliding_window_view(race_fault, window_size, delay)
+    race_fault_measures = np.apply_along_axis(feature_func, 1, race_fault_windows)
+    print("Race fault measures shape:", race_fault_measures.shape)
+
+    # Create features for ball fault data
+    ball_fault_windows = sliding_window_view(ball_fault, window_size, delay) 
+    ball_fault_measures = np.apply_along_axis(feature_func, 1, ball_fault_windows)
+    print("Ball fault measures shape:", ball_fault_measures.shape)
+    
+    size = healthy_measures.shape[0]
+    # Label the data
+    healthy_labels = np.zeros(size)
+    race_fault_labels = np.ones(size)
+    ball_fault_labels = np.ones(size) * 2
+    
+    print("Labels shape:", (healthy_labels.shape, race_fault_labels.shape, ball_fault_labels.shape))
+    
+    # Combine the datasets
+    X = np.vstack((healthy_measures, race_fault_measures, ball_fault_measures))
+    y = np.hstack((healthy_labels, race_fault_labels, ball_fault_labels))
+    print("Concatenated dataset shape:", X.shape)
+    
+    # Split into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    print("Training set shape:", X_train.shape, "Testing set shape:", X_test.shape)
+    
+    return X_train, X_test, y_train, y_test
+"""
