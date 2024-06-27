951bebbb9218eb8e256cbfbd1974bad39836c9a2
martinaKiewek <77066406+martinaKiewek@users.noreply.github.com>
2024-06-27 10:08:23 +0200

    update GUI classifier, color code plotting, funcitoning RQA plot

--- GUI classifier/data_feed.py
+++ GUI classifier/data_feed.py
@@ -6,7 +6,12 @@ import math
 def make_window(path, time, window_size):
     # data = np.genfromtxt(path, delimiter=',', skip_header=1, filling_values=np.nan)
     csv_data = pd.read_csv(path)
-    data = np.array(csv_data['X100_FE_time'])
+    # data = np.array(csv_data['X100_FE_time'])
+    # data = np.array(csv_data['point'])
+    # labels = np.array(csv_data['source'])
+    data = csv_data.iloc[:,0]
+    labels = csv_data.iloc[:, 1]
+
 
     data_max = np.max(data)
     norm_data = data / data_max
@@ -17,7 +22,7 @@ def make_window(path, time, window_size):
     data_range_start = int(time * sampling_rate - window_size)
     data_range_stop = int(time * sampling_rate)
 
-    return norm_data[data_range_start:data_range_stop]
+    return norm_data[data_range_start:data_range_stop], labels[data_range_start:data_range_stop]
 
 # def feed_data(path):
 #     # data = np.genfromtxt(path, delimiter=',', skip_header=1, filling_values=np.nan)
--- datasets/classefierguicsv.py
+++ datasets/classefierguicsv.py
@@ -8,9 +8,9 @@ Created on Wed Jun 26 13:04:53 2024
 import pandas as pd
 
 # Load the CSV files
-file1 = pd.read_csv('normal_3hp_1730rpm.csv')
-file2 = pd.read_csv('.007_ball.csv')
-file3 = pd.read_csv('.007_inner_race.csv')
+file1 = pd.read_csv('/Users/martina/Documents/GitHub/Recurrence_project/Classifier/data/normal_3hp_1730rpm.csv')
+file2 = pd.read_csv('/Users/martina/Documents/GitHub/Recurrence_project/Classifier/data/.007_ball.csv')
+file3 = pd.read_csv('/Users/martina/Documents/GitHub/Recurrence_project/Classifier/data/.007_inner_race.csv')
 #file4 = pd.read_csv('file4.csv')  # not used in this example, but loaded anyway
 
 # Select the desired points from each file
@@ -18,15 +18,17 @@ file3 = pd.read_csv('.007_inner_race.csv')
 points = []
 sources = []
 
-points.extend(file1.iloc[50000:60000, 0].tolist())
-points.extend(file2.iloc[55000:59000, 0].tolist())
-points.extend(file1.iloc[62000:80000, 0].tolist())
-points.extend(file3.iloc[52000:72000, 0].tolist())
+points.extend(file1.iloc[50000:100000, 0].tolist())
+sources.extend(['1'] * len(file1.iloc[50000:100000, 0]))
 
-sources.extend(['1'] * 10000)
-sources.extend(['2'] * 4000)
-sources.extend(['1'] * 18000)
-sources.extend(['3'] * 20000)
+points.extend(file2.iloc[55000:100000, 0].tolist())
+sources.extend(['2'] * len(file2.iloc[55000:100000, 0]))
+
+points.extend(file1.iloc[100000:200000, 0].tolist())
+sources.extend(['1'] * len(file1.iloc[100000:200000, 0]))
+
+points.extend(file3.iloc[52000:100000, 0].tolist())
+sources.extend(['3'] * len(file3.iloc[52000:100000, 0]))
 
 # Create a new DataFrame with the selected points
 df = pd.DataFrame({'point': points, 'source': sources})
