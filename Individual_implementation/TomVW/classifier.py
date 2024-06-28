047bba77bd904a15998bb1086deb0f82adbe905b
Tr0im0n <129655960+Tr0im0n@users.noreply.github.com>
2024-06-28 02:11:58 +0200

    xlsx

--- Individual_implementation/TomVW/classifier.py
+++ Individual_implementation/TomVW/classifier.py
@@ -2,9 +2,10 @@ import os
 
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 
-from Classifier.feature_extraction import calc_rqa_measures
+from Classifier.feature_extraction import calc_rqa_measures, pyrqa
 from Individual_implementation.TomVW.recurrence import view_cdist, threshold
 # pip install openpyxl
 
@@ -26,23 +27,30 @@ def stride_array(array: np.ndarray, window_size: int, step: int):
     return np.lib.stride_tricks.as_strided(array, shape, strides)
 
 
-def rqas_from_time_series(signal: np.ndarray) -> np.ndarray:
-    emb_dim = 5
-    time_delay = 2
-    epsilon = 0.1
+def rqas_from_time_series(signal: np.ndarray, emb_dim: int = 5, time_delay: int = 2,
+                          epsilon: float = 0.1) -> np.ndarray:
     d_matrix = view_cdist(signal, emb_dim, time_delay)
     max_d = np.max(d_matrix)
     r_matrix = threshold(d_matrix, epsilon*max_d)
     return calc_rqa_measures(r_matrix)
 
 
-def csv_to_csv(file_name: str, i: int = 0, n_amount: int = 10_000, window_size: int = 1_000, step: int = 500):
+def my_pyrqa(signal: np.ndarray):
+    emb_dim = 5
+    time_delay = 2
+    epsilon = 0.1
+    return pyrqa(signal, emb_dim, time_delay, epsilon)
+
+
+def csv_to_sheet(file_name: str, i: int = 0, n_amount: int = 10_000, window_size: int = 1_000, step: int = 500):
     print("called")
     df = pd.read_csv(directory_path+file_names[i])
     time_series = df.iloc[:n_amount, 0].to_numpy()
     del df
     strided_time_series = stride_array(time_series, window_size, step)
-    rqas = np.apply_along_axis(rqas_from_time_series, 1, strided_time_series)
+    # rqas = np.apply_along_axis(rqas_from_time_series, 1, strided_time_series)
+    print(my_pyrqa(strided_time_series[0]))
+    rqas = np.apply_along_axis(my_pyrqa, 1, strided_time_series)
     # np.savetxt(r"Figures/data.csv", rqas, delimiter=',')
     print("done with rqas")
     df_rqas = pd.DataFrame(rqas, columns=rqa_names)
@@ -52,11 +60,37 @@ def csv_to_csv(file_name: str, i: int = 0, n_amount: int = 10_000, window_size: 
         df_rqas.to_excel(writer, sheet_name=my_fault_names[i], index=False)
 
 
+def csv_to_xlsx():
+    file_name = "Figures/rqas00.xlsx"
+    for i in range(6):
+        csv_to_sheet(file_name, i, 10_001)
+        print(i)
+
+
+def xlsx_to_png():
+    file_name = "Figures/rqas02.xlsx"
+    dfs = [pd.read_excel(file_name, sheet_name=fault_name) for fault_name in my_fault_names]
+    fig, axs = plt.subplots(2, 3)
+    (ax1, ax2, ax3), (ax4, ax5, ax6) = axs
+    axs_flat = ax1, ax2, ax3, ax4, ax5, ax6
+
+    custom_range = [1, 2, 3, 5, 6, 7]
+    for i, ax in zip(custom_range, axs_flat):
+        for j, df in enumerate(dfs):
+            ax.scatter(df[rqa_names[0]], df[rqa_names[i]], s=4, label=my_fault_names[j])
+        ax.set_xlabel(rqa_names[0])
+        ax.set_ylabel(rqa_names[i])
+        ax.legend()
+
+    plt.show()
+
+
 if __name__ == "__main__":
     os.chdir(r"../..")
-    for i in range(2):
-        csv_to_csv("Figures/rqas00.xlsx", i, 100_000)
-        print(i)
+    csv_to_xlsx()
+    # xlsx_to_png()
+
+
 
 
 """
